d037b602cb923decb9d24c9e130d633024bc0ddb
shayan <shayanpardis82@gmail.com>
2022-02-16 10:45:01 +0330

    remove unused imports

--- src/pages/Dev.js
+++ src/pages/Dev.js
@@ -1,5 +1,3 @@
-import FullCenterPage from "../components/FullCenterPage";
-import Grid from "@mui/material/Grid";
 import Avatar from "@mui/material/Avatar";
 import {Box, useMediaQuery} from "@mui/material";
 import RainbowTypography from "../components/RainbowTypography";
@@ -12,7 +10,7 @@ function Person({name, src, size}) {
             <RainbowTypography variant="h6" textAlign="center">
                 {name}
             </RainbowTypography>
-            <Box sx={{ display: "flex", justifyContent: "center"}} >
+            <Box sx={{display: "flex", justifyContent: "center"}}>
                 <Avatar
                     alt={name}
                     src={src}
@@ -26,35 +24,36 @@ function Person({name, src, size}) {
 export default function Dev() {
     const match = useMediaQuery((theme) => theme.breakpoints.up('sm'));
     const people = [
-        { name: "Shayan Pardis", src:"/images/developers/shayan.jpeg" },
-        { name: "?" },
-        { name: "?" },
-        { name: "Ali Salesi", src:"/images/developers/sales.jpeg" }
+        {name: "Shayan Pardis", src: "/images/developers/shayan.jpeg"},
+        {name: "?"},
+        {name: "?"},
+        {name: "Ali Salesi", src: "/images/developers/sales.jpeg"}
     ]
 
     return (
         <>
             <div>
                 <RainbowTypography variant="h1" textAlign="center">Developers</RainbowTypography>
-                <Spacer space={3} />
+                <Spacer space={3}/>
                 {
                     match ?
-                        <Box sx={{ display: "flex", justifyContent: "space-around" }}>
+                        <Box sx={{display: "flex", justifyContent: "space-around"}}>
                             {
-                                people.map(({name, src}, index)=>
-                                    <div className={`person-${index+1} person`} key={index}>
-                                        <Person index={index} name={name} src={src} size="15vw" />
+                                people.map(({name, src}, index) =>
+                                    <div className={`person-${index + 1} person`} key={index}>
+                                        <Person index={index} name={name} src={src} size="15vw"/>
                                     </div>
                                 )
                             }
                         </Box>
-                    :
-                        <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column" }}>
+                        :
+                        <Box sx={{display: "flex", justifyContent: "center", flexDirection: "column"}}>
                             {
-                                people.map(({name, src}, index)=>
+                                people.map(({name, src}, index) =>
                                     <div className="person">
-                                        <Person index={index} name={name} src={src} size="35vw" className={`person-${index+1} person`} key={index} />
-                                        <Spacer space={3} />
+                                        <Person index={index} name={name} src={src} size="35vw"
+                                                className={`person-${index + 1} person`} key={index}/>
+                                        <Spacer space={3}/>
                                     </div>
                                 )
                             }
@@ -62,7 +61,7 @@ export default function Dev() {
 
                 }
             </div>
-            <Spacer space={10} />
+            <Spacer space={10}/>
         </>
     )
 }
